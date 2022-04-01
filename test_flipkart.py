d83ec1676f7620f04168f60d70c4d5bfee779c6f
3dhanushkrishna <3dhanushkccd@gmail.com>
2022-04-01 14:16:21 +0530

    actions

--- test_flipkart.py
+++ test_flipkart.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pytest
@@ -21,6 +22,18 @@ def test_searchproduct(setUp):
     driver.find_element_by_name("q").send_keys(product)
     time.sleep(1)
     driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[1]/div[2]/div[2]/form/div/button").click()
-    time.sleep(10)
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0, window.scrollY + 1000)")
+    time.sleep(5)
+    s = driver.find_element_by_name("q")
+    action = ActionChains(driver)
+    action.context_click(s)
+    action.perform()
+    time.sleep(5)
+    action.send_keys(Keys.ARROW_DOWN).perform()
+    action.send_keys(Keys.ARROW_DOWN).perform()
+    action.send_keys(Keys.ARROW_DOWN).perform()
+    time.sleep(3)
+
 
 
